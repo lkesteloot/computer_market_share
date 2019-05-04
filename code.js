697d2ed4ac0153d953c67a4513c40968fbe25a28
Lawrence Kesteloot <lk@teamten.com>
2019-05-04 15:27:07 -0700

    Draw year and add slider.

--- code.js
+++ code.js
@@ -1,13 +1,16 @@
 
 var COMPUTER_COUNT = gData[0].length;
-console.log(COMPUTER_COUNT);
 var YEAR_COUNT = gData.length;
 var BAR_HEIGHT = 40;
 var BAR_SPACING = 20;
-var FPS = 10;
+var FPS = 30;
 
 var canvas = document.getElementById("graph");
 var ctx = canvas.getContext("2d");
+var yearSelector = document.getElementById("yearSelector");
+yearSelector.min = gYears[0];
+yearSelector.max = gYears[gYears.length - 1];
+yearSelector.value = gYears[0];
 
 var frame = 0;
 
@@ -26,18 +29,58 @@ var lerp = function(u1, u2, t) {
     }
 };
 
-var graphUnits = function(ctx, values) {
+var graphUnits = function(ctx, year, values) {
     // Clear canvas.
     ctx.fillStyle = "#ffffff";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw year.
+    ctx.fillStyle = "#eeeeee";
+    ctx.font = "300px serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillText(year, canvas.width/2, canvas.height/2);
 
     // Figure out max sales.
     var maxUnits = 0;
     for (var i = 0; i < COMPUTER_COUNT; i++) {
         maxUnits = Math.max(maxUnits, values[i]);
     }
+    if (maxUnits == 0) {
+        // No data.
+        return;
+    }
     var unitsPerPixel = maxUnits/canvas.width;
 
+    // Figure out grid parameters.
+    var zeros = Math.floor(Math.log10(maxUnits));
+    var majorStep = Math.pow(10, zeros);
+    var minorStep = majorStep/10;
+
+    // Figure out grid colors.
+    let dark = (majorStep - maxUnits/10)/(maxUnits*0.9);
+    let majorGray = Math.max(Math.min(127, Math.floor((1 - dark)*128)), 0);
+    let minorGray = majorGray + 128;
+    let majorColor = "rgb(" + majorGray + "," + majorGray + "," + majorGray + ")";
+    let minorColor = "rgb(" + minorGray + "," + minorGray + "," + minorGray + ")";
+
+    // Draw grid.
+    var i = 0;
+    while (true) {
+        let units = i*minorStep;
+        if (units > maxUnits) {
+            break;
+        }
+        let x = units/unitsPerPixel;
+        ctx.strokeStyle = i % 10 == 0 ? majorColor : minorColor;
+        ctx.beginPath();
+        ctx.moveTo(x, 0);
+        ctx.lineTo(x, canvas.height);
+        ctx.stroke();
+        i += 1;
+    }
+
+    // Draw bars and labels.
     ctx.font = "30px serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
@@ -71,7 +114,8 @@ var update = function() {
     var row = Math.floor(frame/FPS);
     var t = frame%FPS/FPS;
     var values = lerp(gData[row], gData[row + 1], t);
-    graphUnits(ctx, values);
+    yearSelector.value = gYears[row];
+    graphUnits(ctx, gYears[row], values);
     if (frame < (YEAR_COUNT - 1)*FPS) {
         frame += 1;
     }
